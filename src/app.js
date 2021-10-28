b839e3657dd108a0108559d6ae3f8e7cad7db9f0
Alwin97 <2111Am1997>
2021-10-28 14:51:08 +0200

    [FEATURE][API_CONNECTION]
    - created example call to our service that itself makes a call to the spoonacular API.
    - installed some npm packages

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 // importing/ requiring packages
 const express = require("express");
+const bodyParser = require("body-parser");
+const fetch = require("node-fetch");
+
+// setting up global variables
+const apiKeyAlwin = '?apiKey=397d585aa35c4b1b8b27beda022fd95f';
+const apiUrl = 'https://api.spoonacular.com/'
 
 // creating express server
 const app = express();
 
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.json())
+
 // set port to listen for requests
 const PORT = process.env.PORT || 8080;
+
+app.post('/produkt', async (req, res) => {
+  const url = `${apiUrl}food/products/upc/${req.body.upc}${apiKeyAlwin}`
+  const fetch_response = await fetch(url);
+  const json = await fetch_response.json();
+  res.json(json);
+})
 
 app.listen(PORT, () => {
   console.log(`app listening at http://localhost:${PORT}`)
