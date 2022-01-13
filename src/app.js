09a1aa0343aa9226152164d58d34c8fe0c9cb8bf
Alwin97 <2111Am1997>
2022-01-13 17:10:11 +0100

    [FEATURE][Create_group]
    - added api call to create a new group
    - added group model

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const fetch = require("node-fetch");
 const mongoose = require("mongoose");
+const Group = require("../models/group");
 
 // setting up global variables
 const apiKeyAlwin = '?apiKey=397d585aa35c4b1b8b27beda022fd95f';
@@ -27,9 +28,18 @@ mongoose.connect(databaseUri)
   })
   .catch(err => console.log(err));
 
+// request that creates a group
+app.post('/group', async (req, res) => {
+  const userID = req.body.userID;
+  const groupName = req.body.groupName;
+
+  new Group( {groupName: groupName, admin: userID}).save().then(() => {
+    res.status(201).json({message: 'group was created'});
+  })
+})
 
 app.post('/produkt', async (req, res) => {
-  const url = `${apiUrl}food/products/upc/${req.body.upc}${apiKeyAlwin}`
+  const url = `${apiUrl}food/products/upc/${req.body.upc}${apiKeyAlwin}`;
   const fetch_response = await fetch(url);
   const json = await fetch_response.json();
   res.json(json);
